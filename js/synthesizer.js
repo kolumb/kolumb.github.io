a916a91d7396ccfb74ea990966a34c3dfe3ce41e
kolumb <kolumbetko@gmail.com>
2014-07-12 16:43:17 +0300

    add musical symbols

--- js/synthesizer.js
+++ js/synthesizer.js
@@ -15,9 +15,13 @@ var volume = document.getElementById('volume');
 var note1 = document.getElementById('note1');
 var note2 = document.getElementById('note2');
 var note3 = document.getElementById('note3');
+var dies1 = document.getElementById('dies1');
+var dies2 = document.getElementById('dies2');
+var dies3 = document.getElementById('dies3');
 var frequencyRange = document.getElementById('frequencyRange');
 var frequencyDisplay = document.getElementById('frequencyDisplay');
 var buttonStop = document.getElementById('buttonStop');
+var cursorClick = 0;
 var cnv = document.getElementById('cnv');
 var ctx = cnv.getContext('2d');
 
@@ -27,25 +31,37 @@ var minor = 0;
 var duration = 0.03; //canvas seconds;
 var amplitude;
 var divider;
-var result = 0;
-var keys = [0, 27.5000, 29.1352, 30.8677, 32.7032, 34.6478, 36.7081, 38.8909,
-	41.2034, 43.6535, 46.2493, 48.9994, 51.9131, 55.0000, 58.2705, 61.7354,
-	65.4064, 69.2957, 73.4162, 77.7817, 82.4069, 87.3071, 92.4986, 97.9989,
-	103.8262, 110.0000, 116.5409, 123.4708, 130.8128, 138.5913, 146.8324,
-	155.5635, 164.8138, 174.6141, 184.9972, 195.9977, 207.6523, 220.0000,
-	233.0819, 246.9417, 261.6256, 277.1826, 293.6648, 311.1270, 329.6276,
-	349.2282, 369.9944, 391.9954, 415.3047, 440.0000, 466.1638, 493.8833,
-	523.2511, 554.3653, 587.3295, 622.2540, 659.2551, 698.4565, 739.9888,
-	783.9909, 830.6094, 880.0000, 932.3275, 987.7666, 1046.5023,
-	1108.7305, 1174.6591, 1244.5079, 1318.5102, 1396.9129, 1479.9777,
-	1567.9817, 1661.2188, 1760.0000, 1864.6550, 1975.5332, 2093.0045,
-	2217.4610, 2349.3181, 2489.0159, 2637.0205, 2793.8259,
-	2959.9554, 3135.9635, 3322.4376, 3520.0000, 3729.3101,
-	3951.0664, 4186.0090, 4434.9220, 4698.6362, 4978.0317, 5274.0409,
-	5587.6516, 5919.9107, 6271.9269
-];
+var result = 27.5;
+var frequencies = [100];
+frequencies[0] = 0;
+frequencies[1] = 27.5;
+var multiplier = Math.pow(2, 1 / 12);
+for (var i = 2; i < 100; i++) {
+	result *= multiplier;
+	frequencies[i] = result;
+}
+var notes = {};
+for (var i = 1; i < 100; i++) {
+	register[i] = {};
+	note = notes[i];
+	note.frequency = frequencies[i];
+	note.octave = Math.floor((i + 8) / 12);
+	note.octavekey = (i + 8) % 12;
+	note.dies = false;
+	if (note.octavekey===1 || 
+		note.octavekey===3 || 
+		note.octavekey===6 || 
+		note.octavekey===8 || 
+		note.octavekey===10) {
+		note.dies = true;
+	}else{
+		note.dies = false;
+	}
+}
+console.log(register["0"]);
+console.log(register["1"]);
+console.log(register["2"]);
 var plaingKeys = [88];
-var plaingFrequencies = [];
 var frequency;
 var frequency2;
 var frequency3;
@@ -103,12 +119,24 @@ function play(event, keynumber) {
 	//init
 	if (event) {
 		keynumber = Number(event.target.dataset.key);
+		if (event.shiftKey) {
+			minorChord.checked = true;
+			minor = -1;
+		} else {
+			minorChord.checked = false;
+			minor = 0;
+		}
+		cursorClick = keynumber;
 		if (isNaN(keynumber)) return;
 	}
 	third = false;
 	fifth = false;
+	frequency = frequencies[keynumber];
+	frequencyDisplay.value = frequency;
+	frequencyRange.value = frequency;
 
-	//update	
+	//update		
+	buttons[keynumber].classList.add("pressed");
 	gainNode.gain.value = volume.value / 100;
 
 	//create
@@ -121,168 +149,183 @@ function play(event, keynumber) {
 	octavekey = (keynumber + 8) % 12;
 	adder = octavekey > 4 ? 1 : 0;
 	adder = octavekey === 11 ? 2 : adder;
-	shift = Math.floor((octavekey + adder) * .5)/2 ;
+	shift = Math.floor((octavekey + adder) * .5) / 2;
 	// 0 1 2 3 4 5 6 7 8 9 10 11
 	// 0 0 1 1 2 3 3 4 4 5 5  6
-
-	note1.style.left = keynumber * .86 + 'em';
-	note1.style.top = (20.5 - octave*3.5 - shift) + 'em';
-
-		oscillator.type = 0; oscillator.connect(gainNode);
-
-		if (majorThird.checked) {
-			oscillator2 = context.createOscillator();
-			oscillator2.type = 0;
-			oscillator2.connect(gainNode);
-		}
-
-		if (perfectFifth.checked) {
-			oscillator3 = context.createOscillator();
-			oscillator3.type = 0;
-			oscillator3.connect(gainNode);
-		}
-
-
-
-		buttons[keynumber].classList.add("pressed");
-
-		frequency = keys[keynumber]; frequencyDisplay.value = frequency; frequencyRange.value = frequency; oscillator.frequency.value = frequency; oscillator.start(0);
-
-		if (majorThird.checked) {
-			third = true;
-			buttons[keynumber + 4 + minor].classList.add("pressed");
-			frequency2 = keys[keynumber + 4 + minor];
-			oscillator2.frequency.value = frequency2;
-			oscillator2.start(0);
-			plaingKeys[keynumber + 4 + minor] = oscillator;
-		}
-
-		if (perfectFifth.checked) {
-			fifth = true;
-			buttons[keynumber + 7].classList.add("pressed");
-			frequency3 = keys[keynumber + 7];
-			oscillator3.frequency.value = frequency3;
-			oscillator3.start(0);
-			plaingKeys[keynumber + 7] = oscillator;
-		}
-
-		drawSin(frequency, frequency2, frequency3);
+	if (shift !== 0) {
+		dies1.style.visibility = "visible";
+		dies1.style.left = (octave * 10.5 + octavekey * .86) - 8.1 + 'em';
+		dies1.style.top = (23.1 - octave * 3.5 - shift) + 'em';
+	} else {
+		dies1.style.visibility = "hidden";
 	}
 
-	function stop(event, note) {
-		oscillator.disconnect();
-		if (event !== null) note = Number(event.target.dataset.key);
-		if (note) {
-			buttons[note].classList.remove("pressed");
-			nodes[note].disconnect();
-			if (majorThird.checked) {
-				buttons[note + 4 + minor].classList.remove("pressed");
-				oscillator2.disconnect();
-			}
-			if (perfectFifth.checked) {
-				buttons[note + 7].classList.remove("pressed");
-				oscillator3.disconnect();
-			}
-		}
+	note1.style.left = (octave * 10.5 + octavekey * .86) - 7.5 + 'em';
+	note1.style.top = (20.5 - octave * 3.5 - shift) + 'em';
+
+	oscillator.type = 0;
+	oscillator.connect(gainNode);
+	oscillator.frequency.value = frequency;
+	oscillator.start(0);
+
+	if (majorThird.checked) {
+		oscillator2 = context.createOscillator();
+		oscillator2.type = 0;
+		oscillator2.connect(gainNode);
+		third = true;
+		buttons[keynumber + 4 + minor].classList.add("pressed");
+		frequency2 = frequencies[keynumber + 4 + minor];
+		oscillator2.frequency.value = frequency2;
+		oscillator2.start(0);
+		plaingKeys[keynumber + 4 + minor] = oscillator;
+
+
+		octave = Math.floor((keynumber + 12 + minor) / 12);
+		octavekey = (keynumber + 12 + minor) % 12;
+		adder = octavekey > 4 ? 1 : 0;
+		adder = octavekey === 11 ? 2 : adder;
+		shift = Math.floor((octavekey + adder) * .5) / 2;
+		note2.style.left = (octave * 10.5 + octavekey * .86) - 7.5 + 'em';
+		note2.style.top = (20.5 - octave * 3.5 - shift) + 'em';
 	}
 
-	function drawSin(freq, freq2, freq3) {
-		amplitude = cnv.height / 2;
-		divider = 1;
-		r2 = r3 = 0;
-		result = 0;
-		ctx.clearRect(0, 0, cnv.width, cnv.height);
+	if (perfectFifth.checked) {
+		oscillator3 = context.createOscillator();
+		oscillator3.type = 0;
+		oscillator3.connect(gainNode);
+		fifth = true;
+		buttons[keynumber + 7].classList.add("pressed");
+		frequency3 = frequencies[keynumber + 7];
+		oscillator3.frequency.value = frequency3;
+		oscillator3.start(0);
+		plaingKeys[keynumber + 7] = oscillator;
 
-		ctx.strokeStyle = "rgba(0,0,0,1)";
+		octave = Math.floor((keynumber + 15) / 12);
+		octavekey = (keynumber + 15) % 12;
+		adder = octavekey > 4 ? 1 : 0;
+		adder = octavekey === 11 ? 2 : adder;
+		shift = Math.floor((octavekey + adder) * .5) / 2;
+		note3.style.left = (octave * 10.5 + octavekey * .86) - 7.5 + 'em';
+		note3.style.top = (20.5 - octave * 3.5 - shift) + 'em';
+	}
+
+	drawSin(frequency, frequency2, frequency3);
+}
+
+function stop(event, note) {
+	oscillator.disconnect();
+	if (event !== null) note = cursorClick; //Number(event.target.dataset.key);
+	if (note) {
+		buttons[note].classList.remove("pressed");
+		nodes[note].disconnect();
+		if (majorThird.checked) {
+			buttons[note + 4 + minor].classList.remove("pressed");
+			oscillator2.disconnect();
+		}
+		if (perfectFifth.checked) {
+			buttons[note + 7].classList.remove("pressed");
+			oscillator3.disconnect();
+		}
+	}
+}
+
+function drawSin(freq, freq2, freq3) {
+	amplitude = cnv.height / 2;
+	divider = 1;
+	r2 = r3 = 0;
+	result = 0;
+	ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+	ctx.strokeStyle = "rgba(0,0,0,1)";
+	ctx.beginPath();
+	ctx.moveTo(0, amplitude);
+	r1 = Math.PI * 2 * freq * duration / cnv.width;
+	if (third) {
+		r2 = Math.PI * 2 * freq2 * duration / cnv.width;
+		divider++;
+	}
+	if (fifth) {
+		r3 = Math.PI * 2 * freq3 * duration / cnv.width;
+		divider++;
+	}
+	for (i = 0; i <= cnv.width; i++) {
+		result = Math.sin(r1 * i);
+		if (third) result += Math.sin(r2 * i);
+		if (fifth) result += Math.sin(r3 * i);
+		ctx.lineTo(i, (1 - result / divider) * amplitude);
+	}
+	ctx.stroke();
+
+	if (third || fifth) {
+		ctx.strokeStyle = "rgba(150,0,0,0.3)";
 		ctx.beginPath();
 		ctx.moveTo(0, amplitude);
-		r1 = Math.PI * 2 * freq * duration / cnv.width;
-		if (third) {
-			r2 = Math.PI * 2 * freq2 * duration / cnv.width;
-			divider++;
-		}
-		if (fifth) {
-			r3 = Math.PI * 2 * freq3 * duration / cnv.width;
-			divider++;
-		}
 		for (i = 0; i <= cnv.width; i++) {
-			result = Math.sin(r1 * i);
-			if (third) result += Math.sin(r2 * i);
-			if (fifth) result += Math.sin(r3 * i);
-			ctx.lineTo(i, (1 - result / divider) * amplitude);
+			ctx.lineTo(i, amplitude - Math.sin(r1 * i) * amplitude / divider);
 		}
 		ctx.stroke();
-
-		if (third || fifth) {
-			ctx.strokeStyle = "rgba(150,0,0,0.3)";
-			ctx.beginPath();
-			ctx.moveTo(0, amplitude);
-			for (i = 0; i <= cnv.width; i++) {
-				ctx.lineTo(i, amplitude - Math.sin(r1 * i) * amplitude / divider);
-			}
-			ctx.stroke();
-		}
-
-		if (third) {
-			ctx.strokeStyle = "rgba(0,150,0,0.3)";
-			ctx.beginPath();
-			ctx.moveTo(0, amplitude);
-			for (i = 0; i <= cnv.width; i++) {
-				ctx.lineTo(i, amplitude - Math.sin(r2 * i) * amplitude / divider);
-			}
-			ctx.stroke();
-		}
-
-		if (fifth) {
-			ctx.strokeStyle = "rgba(0,0,150,0.3)";
-			ctx.beginPath();
-			ctx.moveTo(0, amplitude);
-			for (i = 0; i <= cnv.width; i++) {
-				ctx.lineTo(i, amplitude - Math.sin(r3 * i) * amplitude / divider);
-			}
-			ctx.stroke();
-		}
 	}
 
-	function playRange(event) {
-		if (!rangeOscillator) {
-			rangeOscillator = context.createOscillator();
-			rangeOscillator.type = 0;
-			rangeOscillator.connect(gainNode);
-			rangeOscillator.start(0);
+	if (third) {
+		ctx.strokeStyle = "rgba(0,150,0,0.3)";
+		ctx.beginPath();
+		ctx.moveTo(0, amplitude);
+		for (i = 0; i <= cnv.width; i++) {
+			ctx.lineTo(i, amplitude - Math.sin(r2 * i) * amplitude / divider);
 		}
-		if (event.target.type === "range") {
-			rangeOscillator.frequency.value = frequencyRange.value;
-			frequencyDisplay.value = frequencyRange.value + " Гц";
-		} else {
-			rangeOscillator.frequency.value = parseFloat(frequencyDisplay.value);
-			frequencyRange.value = parseFloat(frequencyDisplay.value);
-		}
+		ctx.stroke();
 	}
 
-	function stopRange(event) {
-		//rangeOscillator.stop(0);
-		rangeOscillator.disconnect();
-		rangeOscillator = null;
+	if (fifth) {
+		ctx.strokeStyle = "rgba(0,0,150,0.3)";
+		ctx.beginPath();
+		ctx.moveTo(0, amplitude);
+		for (i = 0; i <= cnv.width; i++) {
+			ctx.lineTo(i, amplitude - Math.sin(r3 * i) * amplitude / divider);
+		}
+		ctx.stroke();
 	}
+}
 
-	cnv.width = document.body.clientWidth - 20;
-	cnv.height = 256;
-	duration = cnv.width * 0.00003;
+function playRange(event) {
+	if (!rangeOscillator) {
+		rangeOscillator = context.createOscillator();
+		rangeOscillator.type = 3;
+		rangeOscillator.connect(gainNode);
+		rangeOscillator.start(0);
+	}
+	if (event.target.type === "range") {
+		rangeOscillator.frequency.value = frequencyRange.value;
+		frequencyDisplay.value = frequencyRange.value + " Гц";
+	} else {
+		rangeOscillator.frequency.value = parseFloat(frequencyDisplay.value);
+		frequencyRange.value = parseFloat(frequencyDisplay.value);
+	}
+}
+
+function stopRange(event) {
+	//rangeOscillator.stop(0);
+	rangeOscillator.disconnect();
+	rangeOscillator = null;
+}
+
+cnv.width = document.body.clientWidth - 20;
+cnv.height = 256;
+duration = cnv.width * 0.00003;
 
 
 
-	keyboard.addEventListener('mousedown', play, false);
-	keyboard.addEventListener('mouseup', stop, false);
-	frequencyRange.addEventListener('change', playRange, false);
-	buttonStart.addEventListener('click', playRange, false);
-	buttonStop.addEventListener('click', stopRange, false);
-	volume.addEventListener('change', function() {
-		gainNode.gain.value = volume.value / 100;
-	}, false);
+keyboard.addEventListener('mousedown', play, false);
+keyboard.addEventListener('mouseup', stop, false);
+frequencyRange.addEventListener('change', playRange, false);
+buttonStart.addEventListener('click', playRange, false);
+buttonStop.addEventListener('click', stopRange, false);
+volume.addEventListener('change', function() {
+	gainNode.gain.value = volume.value / 100;
+}, false);
 
-	var keyboardspoiler = document.getElementById("keyboardspoiler");
-	var keyboardinfo = document.getElementById("keyboardinfo");
-	keyboardspoiler.addEventListener('click', function(event) {
-		keyboardinfo.classList.toggle("show");
-	}, false);
+var keyboardspoiler = document.getElementById("keyboardspoiler");
+var keyboardinfo = document.getElementById("keyboardinfo");
+keyboardspoiler.addEventListener('click', function(event) {
+	keyboardinfo.classList.toggle("show");
+}, false);
