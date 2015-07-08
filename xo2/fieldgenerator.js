6c2faafd942164f9157550bd82e47f570f088d8e
Danil Kolumbet <kolumbetko@gmail.com>
2015-07-08 22:06:59 +0300

    fix spelling

--- xo2/fieldgenerator.js
+++ xo2/fieldgenerator.js
@@ -1,7 +1,7 @@
 var field = new HTMLCreator();
-
+var fieldArray = [];
 var mainCounter = 0;
-field.addTag('table', 'gamefield', 'nonselect');
+field.addTag('table', 'gamefield', 'noselect');
 for (var i = 0; i < 3; i++) {
 	field.addTag('tr');
 	for (var j = 0; j < 3; j++) {
@@ -11,8 +11,11 @@ for (var i = 0; i < 3; i++) {
 		for (var k = 0; k < 3; k++) {
 			field.addTag('tr');
 			for (var l = 0; l < 3; l++) {				
-				field.addTag('td', mainCounter + 'col' + k + l);
-				field.addText(mainCounter*(k+1)*(l+1));
+				field.addTag('td');
+				field.addText(mainCounter*(k*3 + l+1));
+				field.addTag('div', 'cell' + (27*i + 9*j + 3*k + l), 'over');
+				fieldArray[27*i + 9*j + 3*k + l] = mainCounter*(k*3 + l+1);
+				field.closeTag();
 				field.closeTag();
 			};
 			field.closeTag();
@@ -25,37 +28,4 @@ for (var i = 0; i < 3; i++) {
 field.closeTag();
 
 field.log();
-
-
-
-
-
-/*
-
-var mainCounter = 0;
-field.addTag('table', 'gamefield', 'nonselect');
-for (var i = 0; i < 3; i++) {
-	field.addTag('tr', 'mainrow' + i);
-	for (var j = 0; j < 3; j++) {
-		field.addTag('td', 'maincol' + i + j);
-		field.addTag('table');
-		mainCounter++;
-		for (var k = 0; k < 3; k++) {
-			field.addTag('tr', 'row' + j + k);
-			for (var l = 0; l < 3; l++) {				
-				field.addTag('td', mainCounter + 'col' + k + l);
-				field.addText(mainCounter*(k+1)*(l+1))
-				field.closeTag();
-			};
-			field.closeTag();
-		};
-		field.closeTag();
-		field.closeTag();
-	}
-	field.closeTag();
-};
-field.closeTag();
-
-field.log();
-
-*/
+console.log("fieldArray = " + fieldArray);
