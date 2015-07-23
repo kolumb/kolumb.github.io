0f0078c476d60181133b38b0554dc1b7cc9cf002
Danil Kolumbet <kolumbetko@gmail.com>
2015-07-23 17:48:25 +0300

    add tic-tac-toe game logic

--- xo2/xo2.js
+++ xo2/xo2.js
@@ -9,10 +9,54 @@ var fieldArray =
  8,16,24,32,40,48,56,64,72, 
  9,18,27,36,45,54,63,72,81];
 
-var productCoutArray = 
-[0,1,2,2,3,2,4,2,4,3,2,0,4,0,2,2,3,0,4,0,2,2,0,0,4,1,0,2,2,0,2,0,2,0,0,2,3,0,0,0,2,0,2,0,0,2,0,0,2,1,0,0,0,0,2,0,2,0,0,0,0,0,0,2,1,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,1];
+var score = {};
+score.tic = 
+ [0,0,0,
+  0,0,0,
+  0,0,0];
+score.tac = 
+ [0,0,0,
+  0,0,0,
+  0,0,0];
 
-var linksArray = [
+score.big = {};
+score.big.tic = 0;
+score.big.tac = 0;
+
+score.names = {};
+score.names.tic = 'Крестики';
+score.names.tac = 'Нолики';
+
+var capturedCell = 
+[0,0,0,0,0,0,0,0,0,
+ 0,0,0,0,0,0,0,0,0,
+ 0,0,0,0,0,0,0,0,0,
+ 0,0,0,0,0,0,0,0,0,
+ 0,0,0,0,0,0,0,0,0,
+ 0,0,0,0,0,0,0,0,0,
+ 0,0,0,0,0,0,0,0,0,
+ 0,0,0,0,0,0,0,0,0];
+
+var capturedField = 
+ [false,false,false,
+  false,false,false,
+  false,false,false];
+
+var checks = [448,56,7,292,146,73,273,84];
+/* Minimal binary state of field in case of victory.
+Horizontal:
+111 000 000 = 448
+000 111 000 = 56
+000 000 111 = 7
+Vertical:
+100 100 100 = 292
+010 010 010 = 146
+001 001 001 = 73
+Diagonal:
+100 010 001 = 273
+001 010 100 = 84 */
+
+var linksToCells = [
 [],[0],[1,9],[2,18],[3,10,27],[4,36],[5,11,19,45],[6,54],[7,12,28,63],
 [8,20,72],[13,37],[],[14,21,29,46],[],[15,55],[22,38],[16,30,64],[],
 [17,23,47,73],[],[31,39],[24,56],[],[],[25,32,48,65],[40],[],
@@ -23,73 +67,114 @@ var linksArray = [
 [62,78],[70],[],[],[],[],[],[],[],
 [71,79],[],[],[],[],[],[],[],[],[80] ];
 
+var checkWin = function(fieldSum){
+	for (var i = 0; i < 9; i++) {
+		if((fieldSum & checks[i]) === checks[i]){
+			return true;
+		}
+	};
+	return false;
+};
 
-gamefield.addEventListener('click', function (event) {
-	
-}, false);
-var tic = true;
-var moves = [];
-var makeTurn = function (){
-	var product = first * second;
-	if(moves.indexOf(product) !== -1) {
-		console.log("Пропуск хода.");
-		tic = !tic;
-		return;
+var mark = function (node) {
+	var selectedId = node.id.slice(4);
+	var smallFieldIndex = Math.floor(selectedId/9);
+	if(capturedField[smallFieldIndex]) {
+		return false;
 	}
-	moves.push(product);
-	var listOfCells = linksArray[product];
-	var cellNumber = 0;
-	for(var i = 0; i<listOfCells.length; i++){
-		cellNumber = listOfCells[i];
-		if(tic) {
-			window['cell' + cellNumber].classList.add('tic');
-		} else {
-			window['cell' + cellNumber].classList.add('tac');
+	var selectedProduct = fieldArray[selectedId];
+	if (product === selectedProduct && !capturedCell[selectedId]) {
+		capturedCell[selectedId]++;
+		node.classList.add(tictac);
+		var turnAddition = 512>>selectedId%9+1;
+		score[tictac][smallFieldIndex] += turnAddition;
+		if (checkWin(score[tictac][smallFieldIndex])) {
+			window['block' + (smallFieldIndex + 1)].classList.add(tictac + '-full');
+			capturedField[smallFieldIndex] = true;
+			score.big[tictac] += 512>>smallFieldIndex+1;
+			if (checkWin(score.big[tictac])) {
+				alert(score.names[tictac] + ' выиграли!');
+			}
 		}
 	}
-	tic = !tic;
 }
 
-var moving = true;
+gamefield.addEventListener('click', function (event) {
+	mark(event.target);
+}, false);
+
+
+var tictac = 'tac';
+var moves = [];
+var product = 0;
+var automaticPlay = false;
+var makeTurn = function (){
+	tictac = tictac == 'tic' ? 'tac' : 'tic';
+	product = first * second;
+
+	if (automaticPlay) {
+		if(moves.indexOf(product) !== -1) {
+			console.log("Пропуск хода.");
+			tictac = tictac == 'tic' ? 'tac' : 'tic';
+			return;
+		}
+		moves.push(product);
+		var listOfCells = linksToCells[product];
+		for(var i = 0; i<listOfCells.length; i++){
+			mark(window['cell' + listOfCells[i]]);
+		}
+	}	
+}
+
+var movingControl = true;
 
 var first = 0;
 var second = 0;
 
-var controllerHandler = function (event) {
+controller.addEventListener('click', function (event) {
 	var index = event.target.id.slice(-1);
+	var classList = event.target.classList;
 	if (index === 'r') {return;}
-	if (moving) {
-		if(first && second && index !== first && index !== second) {console.log('Выберите, пожалуйста, число для перемещения.'); return;}
+	if (movingControl) {
+		if(first &&
+			second &&
+			index !== first &&
+			index !== second) {
+			console.log('Выберите, пожалуйста, число для перемещения.'); return;
+		}
 		if (first === index) {
-			event.target.classList.remove('first');
+			classList.remove('first');
 			first = 0;
 		} else if (second === index){
-			event.target.classList.remove('second');
+			classList.remove('second');
 			second = 0;
 		} else {
 			first = index;
-			event.target.classList.add('first');
+			classList.add('first');
 		}
 	} else {
 		if (first === index) {
-			event.target.classList.add('second');
+			classList.add('second');
 			second = index;
 		} else if (second === index){
-			event.target.classList.add('first');
+			classList.add('first');
 			first = index;
 		} else {
 			if (first) {
 				second = index;
-				event.target.classList.add('second');
+				classList.add('second');
 			} else {
 				first = index;
-				event.target.classList.add('first');
+				classList.add('first');
 			}
 		}
 		makeTurn();
 	}
-	moving = !moving;
-}
+	movingControl = !movingControl;
+}, false);
 
 
-controller.addEventListener('click', controllerHandler, false);
+
+automatic.addEventListener('click', function (event) {
+	automaticPlay = event.target.checked ? true : false;	
+}, false);
